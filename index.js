09464931c0ff24019930eff35d6ea281dd743622
TangkaGoswamiPuja <web4dev2learning0@gmail.com>
2024-06-20 12:30:17 +0600

    delete

--- index.js
+++ index.js
@@ -58,6 +58,12 @@ async function run() {
       const result= await slotsCollection.insertOne(slotItem);
       res.send(result)
     })
+    app.delete('/slots/:id' ,async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+  const result= await slotsCollection.deleteOne(query);
+  res.send(result);
+  })
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
