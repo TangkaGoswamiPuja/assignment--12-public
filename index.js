9a373608b9b3ea471405ae089d9a35f660aad280
TangkaGoswamiPuja <web4dev2learning0@gmail.com>
2024-06-21 09:03:42 +0600

    all users

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
 
 
     // user related api 
+    app.get('/users',async(req,res)=>{
+      const result = await
+      usersCollection.find().toArray();
+      res.send(result);
+  })
+
     app.post('/users',async(req,res)=>{
       const users = req.body;
       const query = {email:users.email}
