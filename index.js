780da83052fdf4cf79bcc99f3122b971ecc0e27a
TangkaGoswamiPuja <web4dev2learning0@gmail.com>
2024-06-22 23:34:53 +0600

    last

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const testCollection = client.db("doctorDoctor").collection('allTest');
     const slotsCollection = client.db("doctorDoctor").collection('slots');
     const usersCollection = client.db("doctorDoctor").collection('users');
@@ -194,7 +194,7 @@ image :item.image
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
